62af3cfd7efbd32ac1f59c5ced4a0b07335ea2d3
Syphdias <syphdias+git@gmail.com>
2022-11-12 23:45:14 +0100

    Initial commit of swapping script
    This adds README, script and picture for swapping two i3/sway containers
    in a specified manner.

---
+++ README.md
@@ -0,0 +1,40 @@
+## About
+This swaps two containers/windows in an i3 workspace: The focused container and
+another one, depending on the picked options.
+Probably works on sway as well.
+
+## Options
+In one picture:
+
+![](assets/selection-visualization.png)
+
+### Layout
+By default it swaps horizontal, but `--layout vertical` should work as well.
+Though I did little testing since it is not that useful to me at the moment.
+
+### Level
+By default it goes up the workspace tree to find the highest horizontally
+split container (depending on layout option).
+You can change it to use the lowest container that is horizontally/vertically
+split.
+
+The is no middle ground. If you have a suggestion how and how it would be
+useful, let me know.
+
+### Swapee
+By default the first container in the "other half" will be picked. With the
+level option we found a common container. Now we need to look at the other half
+and pick a candidate for swaping – the swapee. You can also pick the `--swapee
+last`, if you prefer.
+
+Like with the level option. There is no middle ground. But again, if you know
+how it could be done and how it would be useful, let me know.
+
+### Non-existent option
+Currently it assumes there is only two containers in the common container and it
+picks the other one. So technically there is a option missing between level and
+swapee.
+
+## Disclaimer
+I know this is a horrible name.
+I just wrote this for myself but feel free to report any issues you encounter.
---
+++ i3swap.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python
+from typing import Literal, Optional
+from sys import stderr
+from argparse import ArgumentParser
+from i3ipc import Connection, Con
+
+
+def print_or_nodes(o: Con, depth: int = 0) -> str:
+    name = o.name or ""
+    print(
+        f"{'  '*depth}{o.type}: {o.id} {name:.42} ({o.layout} with {len(o.nodes)} children)"
+    )
+    if o.nodes:
+        for n in o.nodes:
+            print_or_nodes(n, depth + 1)
+
+
+def find_swapee(
+    focused_container: Con,
+    layout: Literal["horizontal", "vertical"],
+    level: Literal["highest", "lowest"],
+    swapee: Literal["first", "last"],
+    verbose: int = 0,
+) -> Optional[Con]:
+    """Return swapee depending on criteria
+
+    workspace: 94403059612560 2 (splith with 1 children)
+      con: 94403059613056  (splith with 1 children)
+        con: 94403059613520  (splith with 1 children)
+          con: 94403059613984  (splith with 1 children)
+            con: 94403058970656  (splith with 1 children)
+              con: 94403059981248  (splith with 1 children)
+                con: 94403060009888  (splith with 2 children)                                                       < common_parent with children(!) (splith, highest)
+                  con: 94403060353904  (splitv with 2 children)
+                    con: 94403059614448 Tilix: vim i3swap.py (splith with 0 children)                               < swapee (splith, highest, first)
+                    con: 94403060624480 Tilix: ~ (splith with 0 children)                                           < swapee (splith, highest, last)
+                  con: 94403060104288  (splitv with 3 children)
+                    con: 94403060153328 Tilix: IPython: home/syphdias (splith with 0 children)
+                    con: 94403059853696  (splith with 2 children)                                                   < common_parent with children(!) (splith, lowest)
+                      con: 94403060640272  (splitv with 2 children)
+                        con: 94403060477552 Tilix: ~ (splith with 0 children)                                       < swapee (splith, lowest, first)
+                        con: 94403060027792 Tilix: ~ (splith with 0 children)                                       < swapee (splith, lowest, last)
+                      con: 94403060474048  (splitv with 3 children)
+                        con: 94403060338064 Tilix: ~/git/private/i3-swap (splith with 0 children)
+                        con: 94403060644688 Tilix: ~/git/private/i3-swap/i3swap.py --l (splith with 0 children)     < focused_container
+                        con: 94403060524208 Tilix: ~ (splith with 0 children)
+                    con: 94403059044416 Tilix: ~ (splith with 0 children)
+    """
+    if verbose >= 2:
+        print_or_nodes(focused_container.workspace())
+
+    if layout == "vertical":
+        layout = "splitv"
+    else:
+        layout = "splith"
+
+    current_container = focused_container
+    # find the parent container containing the focused container
+    # and the one we want to swap with
+    common_parent = None
+    while current_container.type != "workspace":
+        if current_container.layout == layout and len(current_container.nodes) > 1:
+            # we found the FIRST container with the wanted layout
+            # decend in the first node that we did not come from
+            common_parent = current_container
+            if level == "lowest":
+                break
+        if len(current_container.parent.nodes) > 1:
+            focused_half = current_container
+        current_container = current_container.parent
+
+    # return None if no common parent was found
+    if not common_parent:
+        common_parent = focused_half.parent
+
+    # pick either the first (default) or the last container in the swapee half
+    swapee_index = 0
+    if swapee == "last":
+        swapee_index = -1
+
+    # we found the current parent now we need to decent into it
+    # to find the swapee (first container we find)
+    swapee_half = next(
+        (node for node in common_parent.nodes if node != focused_half),
+        None,
+    )
+    while len(swapee_half.nodes) > 0:
+        swapee_half = swapee_half.nodes[swapee_index]
+    swapee = swapee_half
+
+    if verbose >= 2:
+        print("common parent:", common_parent.id)
+        print("  focused half:", focused_half.id)
+        print("  swapee half:", swapee_half.id)
+        print("    swapee:", swapee.id)
+
+    return swapee
+
+
+def main(args) -> None:
+    i3 = Connection()
+    tree = i3.get_tree()
+    focused_container = tree.find_focused()
+
+    swapee = find_swapee(
+        focused_container,
+        layout=args.layout,
+        level=args.level,
+        swapee=args.swapee,
+        verbose=args.verbose,
+    )
+
+    if not swapee:
+        if args.verbose:
+            print("Found no swapee. Nothing to swap.")
+        return
+
+    move_result = focused_container.command(f"swap container with con_id {swapee.id}")
+    if args.verbose:
+        print(move_result[0].ipc_data)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+
+    parser.add_argument(
+        "--layout",
+        default="horizontal",
+        choices=["horizontal", "vertical"],
+        help="Swap horizontally or vertically",
+    )
+    parser.add_argument(
+        "--level",
+        default="highest",
+        choices=["highest", "lowest"],
+        help="Select which hierarchy level you want swap with",
+    )
+    parser.add_argument(
+        "--swapee",
+        default="first",
+        choices=["first", "last"],
+        help="Take first or last container in the swapping container",
+    )
+    parser.add_argument(
+        "--verbose",
+        "-v",
+        action="count",
+        default=0,
+    )
+
+    args = parser.parse_args()
+
+    if args.verbose:
+        print(args)
+
+    main(args)
